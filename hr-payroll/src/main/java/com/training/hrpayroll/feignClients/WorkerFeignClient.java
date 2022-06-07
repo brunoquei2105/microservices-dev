69ce948c76705bbd8a2ff18b995c22cc99609ab9
brunoquei2105 <bruno.queiroz@liferay.com>
2022-06-07 16:21:31 -0300

    Eureka server created

--- hr-payroll/src/main/java/com/training/hrpayroll/feignClients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/training/hrpayroll/feignClients/WorkerFeignClient.java
@@ -1,7 +1,6 @@
 package com.training.hrpayroll.feignClients;
 
 import com.training.hrpayroll.entities.Worker;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
