4bfc0f82b976d2e8c2f7c0563bb00792a4e72340
brunoquei2105 <bruno.queiroz@liferay.com>
2022-06-09 14:38:44 -0300

    Enable eureka client to hr-worker and hr-payroll

--- hr-payroll/src/main/java/com/training/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/training/hrpayroll/HrPayrollApplication.java
@@ -2,6 +2,7 @@ package com.training.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @Configuration
 @EnableFeignClients
-@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
 public class HrPayrollApplication {
 
     public static void main(String[] args) {
