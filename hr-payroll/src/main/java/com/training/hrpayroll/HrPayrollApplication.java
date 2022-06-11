1d3c3cd7cd3263326efe88b2ffa745047c17973b
brunoquei2105 <bruno.queiroz@liferay.com>
2022-06-11 17:55:05 -0300

    Getway Zuul Api created

--- hr-payroll/src/main/java/com/training/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/training/hrpayroll/HrPayrollApplication.java
@@ -2,6 +2,7 @@ package com.training.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @EnableEurekaClient
 @EnableHystrix
+@EnableCircuitBreaker
 public class HrPayrollApplication {
 
     public static void main(String[] args) {
