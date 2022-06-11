524c903a92c44b4739437a949ad12c9059b86a9f
brunoquei2105 <bruno.queiroz@liferay.com>
2022-06-11 20:13:16 -0300

    Api Gateway created

--- hr-worker/src/main/java/com/training/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/training/hrworker/HrWorkerApplication.java
@@ -3,13 +3,15 @@ package com.training.hrworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Configuration
 public class HrWorkerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HrWorkerApplication.class, args);
+    public static void main(String[] args)  {
+        SpringApplication.run(HrWorkerApplication .class, args);
     }
 
 }
